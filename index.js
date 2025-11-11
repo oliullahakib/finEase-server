af5f52579d521bf84ba6c74784ea79d086cb8801
oliullahakib <oliullahakib@gmail.com>
2025-11-11 16:42:45 +0600

    added middleware for verifying firebase token to protect add and my transactions

--- index.js
+++ index.js
@@ -3,58 +3,89 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var admin = require("firebase-admin");
+var serviceAccount = require("./finease-admin-sdk.json");
 
 const app = express()
 const port = process.env.PORT || 3000
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+// custom middleware 
+const verifyFirebaseToken = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized access" })
+    }
+    const token = req.headers.authorization.split(" ")[1]
+    if(!token){
+       return res.status(401).send({ message: "Unauthorized access" }) 
+    }
+
+    // verifying token 
+    try {
+    const decode=  admin.auth().verifyIdToken(token)  
+    req.token_email=decode.email
+    next()
+    } catch {
+       return res.status(401).send({ message: "Unauthorized access" })  
+    }
+}
+
+app.get('/', (req, res) => {
+    res.status
     res.send("server is running")
 })
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wfr9cox.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const db = client.db("FinEase")
-    const transactionsCollection=db.collection("transactions")
-    // handle apis here 
-    app.get("/my-transactions",async(req,res)=>{
-        const email = req.query.email
-        let query={}
-        if(email){
-            query.email=email
-        }
-        const result = await transactionsCollection.find(query).toArray()
-        res.send(result)
-    })
-    app.post('/add-transaction',async(req,res)=>{
-        const newTransaction = req.body
-        const result = await transactionsCollection.insertOne(newTransaction)
-        res.send(result)
-    })
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db("FinEase")
+        const transactionsCollection = db.collection("transactions")
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // handle apis here 
+        app.get("/my-transactions", verifyFirebaseToken, async (req, res) => {
+
+            const email = req.query.email
+            let query = {}
+            if (email) {
+                query.email = email
+                const result = await transactionsCollection.find(query).toArray()
+                return res.send(result)
+            }
+            res.send({ message: "Expect an Email", data: {} })
+        })
+        app.post('/add-transaction',verifyFirebaseToken ,async (req, res) => {
+            const newTransaction = req.body
+            const result = await transactionsCollection.insertOne(newTransaction)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
