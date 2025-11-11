4a472399d0ad178370b6579747e3fdccaf3bf10d
oliullahakib <oliullahakib@gmail.com>
2025-11-11 13:30:05 +0600

    initialize the server with express and cors

---
+++ index.js
@@ -0,0 +1,17 @@
+// dependance 
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("server is running")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
