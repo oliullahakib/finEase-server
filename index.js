381181836e1c74826554653cd040cfc5dbb35fed
oliullahakib <oliullahakib@gmail.com>
2025-11-13 16:12:00 +0600

    host server to vercel

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-var admin = require("firebase-admin");
-var serviceAccount = require("./finease-admin-sdk.json");
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -55,7 +56,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const db = client.db("FinEase")
         const transactionsCollection = db.collection("transactions")
 
@@ -118,8 +119,8 @@ async function run() {
             res.send(result)
         })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
